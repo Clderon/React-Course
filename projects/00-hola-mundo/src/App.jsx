eca45a699d9b8c4c9b8ea411d7381be2661cd914
Clderon <luis.calderon@unas.edu.pe>
2024-03-05 06:16:41 -0500

    Add component 01

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,27 +1,99 @@
 // spell:disable
+import { useState } from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+
+    {
+        userName: 'midudev',
+        name: 'Miguel Angel Duran',
+        isFollowing: true,
+    },
+    {
+        userName:'pheralb',
+        name:'Pablo H.',
+        isFollowing: false,
+    },
+    {
+        userName: 'PacoHdezs',
+        name: 'Paco Hdezs',
+        isFollowing: true,
+        
+    },
+    {
+        userName: 'TMChein',
+        name: 'Tomas',
+        isFollowing: false,
+
+    },
+]
+
+
+
 // creando la function app
 
 export function App(){
 
+    // cada ves que la aplicacion cambio de estado se renderizan todos los elementos nuevamente pero en el DON solo se renderizan los cambios
+    // que se realizaron
+    const [name, setName] = useState('midudev')
+
+    const cambioName = ()=>{
+        setName('Luis Miguel')
+
+    }
+
     return (
         // sin llaves = true en los boolean
         <section className='App'>
-            <TwitterFollowCard 
-                isFollow 
-                userName={'midudev'} 
-                name={'Miguel Angel Duran'}/>
+
+            {
+                users.map(({userName, name, isFollowing}) => (
+                    <TwitterFollowCard
+                        // ciamdo agregamos arreglos se incluye la key
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing = {isFollowing}
+                    >
+                        {/* children */}
+                        {name} 
+                    </TwitterFollowCard>
+                ))
+            }
+
+            {/* 
+                se pueden tener componentes dentro de otros
+                <TwitterFollowCard> 
+                    <TwitterFollowCard>
+
+                    </TwitterFollowCard>
+                </TwitterFollowCard>
+            */}
+            {/* <TwitterFollowCard 
+                userName={name} 
+                initialIsFollowing={true}
+                name={'Miguel Angel Duran'}>
+            </TwitterFollowCard>
             
             <TwitterFollowCard 
-                isFollow={false} 
                 userName={'pheralb'} 
-                name={'Miguel Angel Duran'}/>
+                name={'Miguel Angel Duran'}>
+            </TwitterFollowCard>
             
             <TwitterFollowCard 
-                isFollow={false} 
-                userName={'elonmusk'} 
-                name={'Miguel Angel Duran'}/>
+                userName={'elonmusk'} >
+                 Children (puede incluir mas cosas) 
+                <i>Luis Miguel</i> 
+            </TwitterFollowCard> */}
+
+
+
+            <button onClick={cambioName}>
+                Cambiar nombre
+            </button>
+
+            {/* Cuando utilizar el children ( cuando el componente va a ser muy extensible)  o como props depende de  como se va a componer la ui del usuario*/}
         </section>
     )   
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,19 +1,46 @@
+// importando la utilidad ( hugs ) para poder crear estados
+import { useState } from "react";
 
-export function TwitterFollowCard({userName, name, isFollow}){
-    console.log(isFollow);
+// children (todo lo que envuelve) [ puede cer una cadena de texto o un elemento html]
+// valores por defecto en los parámetros
+export function TwitterFollowCard({children, userName='unknow', name, initialIsFollowing}){
+
+
+    // creando un estado ( como es el estado inicial)
+    // retorna un array de 2 en el [0] retorna el valor del estado [1] una función que permite actualizar el estado para la nueva version
+    // desestructurando el state
+    // es estado inicial solo se puede iniciar 1 vez
+    const [isFollow,setFolloWing ]  = useState(initialIsFollowing)
+
+    // rendering condicional ( dependiendo de una condición que rendering una cosa o otra)
+    // respondiendo a un estado 
+    const text = isFollow ? 'Siguiendo' : 'Seguir';
+    const buttonClassName = isFollow  ? 'tw-followCard-button is-follow' : 'tw-followCard-button'
+
+    const handleClick = () => {
+        // cuando hagamos click en el button vamos a cambiar el button
+        setFolloWing(!isFollow)
+    }
+
+
     return (
+
+
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' src={`https://unavatar.io/${userName}`} alt="avatar de deviantArt" />
+                <img className='tw-followCard-avatar' src={`https://unavatar.io/${ userName     }`} alt="avatar de deviantArt" />
                 <div className='tw-followCard-info'>
-                    <strong className='tw-followCard-infoUsername'>{name}</strong>
-                    <span className='tw-followCard-infoContact'>@{userName}</span>
+                    <strong className='tw-followCard-infoUsername'>{ name||children }</strong>
+                    <span className='tw-followCard-infoContact'>@{ userName }</span>
                 </div>
             </header>
 
 
             <aside className="tw-followCard-aside">
-                <button className='tw-followCard-button'>Seguir</button>
+                <button className= {buttonClassName} onClick={handleClick}>
+                   <span className="tw-followCard-text">{ text }</span>   {/* Children -> lo que envuelve este elemento */}
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+                </button>
             </aside>
         </article>
 
